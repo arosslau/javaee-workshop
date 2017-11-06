a2fcc27c1ee69a063ab35cb2fa3d9669399f3e3f
Dirk Weil <dirk.weil@gedoplan.de>
2017-11-06 15:37:45 +0100

    Repo-Basisklassen

--- backend/src/main/java/de/gedoplan/workshop/persistence/impl/TalkJpaRepository.java
+++ backend/src/main/java/de/gedoplan/workshop/persistence/impl/TalkJpaRepository.java
@@ -1,47 +1,22 @@
 package de.gedoplan.workshop.persistence.impl;
 
+import de.gedoplan.baselibs.persistence.repository.SingleIdEntityRepository;
 import de.gedoplan.workshop.domain.Talk;
 import de.gedoplan.workshop.persistence.TalkRepository;
 
-import java.util.List;
-
 import javax.enterprise.context.ApplicationScoped;
-import javax.inject.Inject;
-import javax.persistence.EntityManager;
 import javax.transaction.Transactional;
 
 @ApplicationScoped
 @Transactional(rollbackOn = Exception.class)
-public class TalkJpaRepository /* extends SingleIdEntityRepository<Integer, Talk> */ implements TalkRepository {
+public class TalkJpaRepository extends SingleIdEntityRepository<Integer, Talk> implements TalkRepository {
 
-  @Inject
-  EntityManager entityManager;
+  // @PersistenceContext(unitName="hugo")
+  // @Override
+  // public void setEntityManager(EntityManager entityManager) {
+  // // TODO Auto-generated method stub
+  // super.setEntityManager(entityManager);
+  // }
 
-  @Override
-  public void persist(Talk talk) {
-    this.entityManager.persist(talk);
-  }
-
-  @Override
-  public Talk merge(Talk talk) {
-    return this.entityManager.merge(talk);
-  }
-
-  @Override
-  public boolean remove(Talk talk) {
-    this.entityManager.remove(talk);
-    // TODO Q&D
-    return true;
-  }
-
-  @Override
-  public Talk findById(Integer id) {
-    return this.entityManager.find(Talk.class, id);
-  }
-
-  @Override
-  public List<Talk> findAll() {
-    return this.entityManager.createQuery("select t from Talk t", Talk.class).getResultList();
-  }
-
+  // List<Talk> findByType(TalkType talkType) { }
 }
