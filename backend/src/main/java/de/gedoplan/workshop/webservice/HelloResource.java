44a4a3a539fa825daf04e1030cf8a8b90b615ad7
Dirk Weil <dirk.weil@gedoplan.de>
2017-11-06 11:07:56 +0100

    .

--- backend/src/main/java/de/gedoplan/workshop/webservice/HelloResource.java
+++ backend/src/main/java/de/gedoplan/workshop/webservice/HelloResource.java
@@ -2,12 +2,21 @@ package de.gedoplan.workshop.webservice;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 
 @Path("hello")
 public class HelloResource {
 
   @GET
+  @Produces(MediaType.TEXT_PLAIN)
   public String sayHello() {
     return "Hello, world";
+  }
+
+  @GET
+  @Produces(MediaType.APPLICATION_JSON)
+  public String sayHelloJson() {
+    return "Hello, world (JSON";
   }
 }
