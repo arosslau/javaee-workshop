28ec2f5aee0d1209451632a5e2a3e075596a5a2f
Dirk Weil <dirk.weil@gedoplan.de>
2017-11-06 11:38:05 +0100

    TalkRepoository als Mock

---
+++ backend/src/main/java/de/gedoplan/workshop/persistence/impl/TalkMockRepository.java
@@ -0,0 +1,65 @@
+package de.gedoplan.workshop.persistence.impl;
+
+import de.gedoplan.workshop.domain.Talk;
+import de.gedoplan.workshop.domain.TalkType;
+import de.gedoplan.workshop.persistence.TalkRepository;
+
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.enterprise.context.ApplicationScoped;
+
+@ApplicationScoped
+public class TalkMockRepository implements TalkRepository {
+
+  private Map<Integer, Talk> store = new HashMap<>();
+  private int idGenerator = 1;
+
+  protected TalkMockRepository() {
+    persist(new Talk("Java EE ohne Ballast", TalkType.WORKSHOP, createDateOf(2017, 11, 6, 9, 0), 480));
+    persist(new Talk("Eroeffnung", TalkType.KEYNOTE, createDateOf(2017, 11, 7, 9, 0), 45));
+    persist(new Talk("DDD mit Java EE", TalkType.SESSION, createDateOf(2017, 11, 7, 15, 0), 75));
+  }
+
+  private static Date createDateOf(int year, int month, int dayOfMonth, int hour, int minute) {
+    return Date.from(ZonedDateTime.of(year, month, dayOfMonth, hour, minute, 0, 0, ZoneId.of("Europe/Berlin")).toInstant());
+  }
+
+  @Override
+  public void persist(Talk talk) {
+    talk.setId(this.idGenerator++);
+    this.store.put(talk.getId(), talk);
+  }
+
+  @Override
+  public Talk merge(Talk talk) {
+    if (talk.getId() == null) {
+      persist(talk);
+    } else {
+      this.store.put(talk.getId(), talk);
+    }
+
+    return talk;
+  }
+
+  @Override
+  public boolean remove(Talk talk) {
+    return this.store.remove(talk.getId()) != null;
+  }
+
+  @Override
+  public Talk findById(Integer id) {
+    return this.store.get(id);
+  }
+
+  @Override
+  public List<Talk> findAll() {
+    return new ArrayList<>(this.store.values());
+  }
+
+}
--- backend/src/main/java/de/gedoplan/workshop/webservice/TalkResource.java
+++ backend/src/main/java/de/gedoplan/workshop/webservice/TalkResource.java
@@ -1,10 +1,11 @@
 package de.gedoplan.workshop.webservice;
 
 import de.gedoplan.workshop.domain.Talk;
-import de.gedoplan.workshop.domain.TalkType;
+import de.gedoplan.workshop.persistence.TalkRepository;
 
-import java.util.Date;
+import java.util.List;
 
+import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
@@ -14,13 +15,20 @@ import javax.ws.rs.core.MediaType;
 @Path("talk")
 public class TalkResource {
 
+  @Inject
+  private TalkRepository talkRepository;
+
+  @GET
+  @Produces({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
+  public List<Talk> getAllTalks() {
+    return this.talkRepository.findAll();
+  }
+
   @Path("{id}")
   @GET
   @Produces({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
   public Talk getTalk(@PathParam("id") Integer id) {
-    Talk talk = new Talk("xxx", TalkType.KEYNOTE, new Date(), 45);
-    talk.setId(id);
-
+    Talk talk = this.talkRepository.findById(id);
     return talk;
   }
 }
